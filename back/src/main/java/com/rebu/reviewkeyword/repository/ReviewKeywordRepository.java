f310406df432d638531c554060276550b94c4120
Lee Yuseung <105223190+yuseung0429@users.noreply.github.com>
2024-08-05 16:28:14 +0900

    Fix #33 ReviewKeyword 주석 제거 (#34)

--- back/src/main/java/com/rebu/reviewkeyword/repository/ReviewKeywordRepository.java
+++ back/src/main/java/com/rebu/reviewkeyword/repository/ReviewKeywordRepository.java
@@ -1,21 +1,21 @@
-//package com.rebu.reviewkeyword.repository;
-//
-//import com.rebu.reviewkeyword.dto.ReviewKeywordCountDto;
-//import com.rebu.reviewkeyword.entity.ReviewKeyword;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//
-//import java.util.List;
-//
-//public interface ReviewKeywordRepository extends JpaRepository<ReviewKeyword, Long> {
-//
-//    @Query(value = """
-//        SELECT new com.rebu.reviewkeyword.dto.ReviewKeywordCountDto(rk.keyword, COUNT(*))
-//        FROM ReviewKeyword rk
-//        INNER JOIN SelectedReviewKeyword srk ON rk.id = srk.reviewKeyword.id
-//        INNER JOIN Review r ON srk.review.id = r.id
-//        WHERE r.shopProfile.id = :profileId
-//        GROUP BY rk.id
-//        """)
-//    List<ReviewKeywordCountDto> countReviewKeywordsByProfileId(Long profileId);
-//}
+package com.rebu.reviewkeyword.repository;
+
+import com.rebu.reviewkeyword.dto.ReviewKeywordCountDto;
+import com.rebu.reviewkeyword.entity.ReviewKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReviewKeywordRepository extends JpaRepository<ReviewKeyword, Long> {
+
+    @Query(value = """
+        SELECT new com.rebu.reviewkeyword.dto.ReviewKeywordCountDto(rk.keyword, COUNT(*))
+        FROM ReviewKeyword rk
+        INNER JOIN SelectedReviewKeyword srk ON rk.id = srk.reviewKeyword.id
+        INNER JOIN Review r ON srk.review.id = r.id
+        WHERE r.shopProfile.id = :profileId
+        GROUP BY rk.id
+        """)
+    List<ReviewKeywordCountDto> countReviewKeywordsByProfileId(Long profileId);
+}
