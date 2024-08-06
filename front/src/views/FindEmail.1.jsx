58200e773c1a722bc9e53b1e777213df9034cc4e
jin <merong2391@gmail.com>
2024-08-06 22:59:11 +0900

    이메일 찾기 기능 완성

--- front/src/views/FindEmail.1.jsx
+++ front/src/views/FindEmail.1.jsx
@@ -1,64 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LoginTitle from "../components/common/LoginTitle";
 import ButtonBack from "../components/common/ButtonBack";
 import axios from "axios";
 import { BASE_URL } from "./Signup";
-import "../views/Login.css";
-import styled, { css } from "styled-components";
 import ButtonLarge from "../components/common/ButtonDisabled";
+import {
+  Container,
+  Div,
+  ErrorText,
+  Timer,
+  SubmitButtonContainer,
+  Button,
+} from "./FindEmail";
 
-const ButtonStyles = css`
-  background-color: #a55eea;
-  border: none;
-  border-radius: 25px;
-  color: white;
-  height: 2rem;
-  padding: auto;
-  cursor: pointer;
-  white-space: nowrap;
-  /*   버튼안에 있는 텍스트가 화면이 줄어들더라도 줄바꿈되지 않도록 설정해줌*/
-`;
-
-const Container = styled.div`
-  align-items: center;
-  margin: 2rem 3rem;
-  height: 100vh;
-`;
-const Div = styled.div`
-  display: flex;
-`;
-const Button = styled.button`
-  ${ButtonStyles}
-  height:3.1rem;
-  margin-top: 0;
-  padding: 1rem;
-  /* margin: 0.7rem; */
-`;
-const Button2 = styled.button`
-  ${ButtonStyles}
-  height:3.1rem;
-  margin-top: 0.15rem;
-  margin-left: 0.7rem;
-`;
-
-const SubmitButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 2rem;
-  white-space: nowrap;
-`;
-const ErrorText = styled.p`
-  color: red;
-  margin: 0;
-  padding: 0.5rem 0;
-`;
-
-const Timer = styled.p`
-  color: red;
-  font-weight: bold;
-`;
-
-const FindEmail = () => {
+export const FindEmail = () => {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [phoneVeri, setPhoneVeri] = useState("");
@@ -72,7 +27,6 @@ const FindEmail = () => {
   const [ptagMessage, setPtagMessage] = useState(
     "인증번호를 문자 메시지로 전송하였습니다"
   ); //p태그 내용
-  const [findEmailResult, setFindEmailResult] = useState("");
 
   const nameChange = (e) => {
     const filteredValue = e.target.value.replace(
@@ -100,7 +54,7 @@ const FindEmail = () => {
     return value;
   };
 
-  // API: 휴대폰인증 (6자리 코드)요청 POST -> 인증코드 폰으로 전송
+  // API: 휴대폰인증(6자리 코드)요청 POST -> 인증코드 폰으로 전송
   const sendPhoneVerification = async () => {
     try {
       const response = await axios.post(`${BASE_URL}/api/auths/phone/send`, {
@@ -108,7 +62,7 @@ const FindEmail = () => {
         purpose: "findEmail",
       });
       console.log(response.data);
-      if (response.data.code === "전화번호 인증 코드 전송 성공") {
+      if (response.data.code === "이메일 인증 코드 전송 성공 코드") {
         alert("인증번호가 발송되었습니다.");
         setIsVerificationFieldVisible(true);
       } else {
@@ -151,27 +105,43 @@ const FindEmail = () => {
     return `${minutes}:${secs < 10 ? `0${secs}` : secs}`;
   };
 
+  const findEmail = async () => {
+    try {
+      const response = await axios.get(
+        `${BASE_URL}/api/members/find-email?name=${name}&phone=${phone}`
+      );
+      if (response.data.body) {
+        alert(`Email: ${response.data.body}`);
+      } else {
+        alert("이메일이 존재하지 않습니다.");
+      }
+    } catch (error) {
+      console.error("Error while finding email:", error);
+      alert("이메일을 찾는 중 오류가 발생했습니다.");
+    }
+  };
+
+  // const phoneVeriChange = (e) => {
+  //   setPhoneVeri(e.target.value);
+  // };
   // 인증번호 확인 input 6자리수 제한
   const phoneVeriCodeChange = (e) => {
-    // const newCode = e.target.value.replace(/[^0-9]/g, "").slice(0, 6); // 숫자만 허용하고 6자리로 제한
-    const newCode = e.target.value.slice(0, 6); // 6자리로 제한
+    const newCode = e.target.value.replace(/[^0-9]/g, "").slice(0, 6); // 숫자만 허용하고 6자리로 제한
     setPhoneVeri(newCode);
     // setEmptyFieldsMsg((prev) => ({ ...prev, phoneVeriCode: false }));
   };
 
-  // API 휴대폰 인증번호 확인 (6자리 코드 휴대폰으로 받은 것 인증)
+  // 코드인증
   const verifyPhoneCode = async () => {
     try {
       const response = await axios.post(`${BASE_URL}/api/auths/phone/verify`, {
         phone: phone,
-        purpose: "findEmail",
+        purpose: "signup",
         verifyCode: phoneVeri,
       });
       console.log(phoneVeri, response.data);
       if (response.data.code === "전화번호 인증 성공 코드") {
         alert("전화번호 인증이 완료되었습니다.");
-        console.log("전화번호 인증이 완료되었습니다.");
-        setPtagMessage("전화번호 인증이 완료되었습니다.");
         setIsCodeVerified(true);
       } else {
         alert("전화번호 인증에 실패했습니다. 사유:" + response.data.code);
@@ -180,35 +150,6 @@ const FindEmail = () => {
     } catch (error) {
       console.error("Error verifying phone code:", error);
       alert("전화번호 인증 중 오류가 발생했습니다.");
-    }
-  };
-
-  // API 이메일 찾기 (GET)
-  const findEmail = async () => {
-    try {
-      const response = await axios.get(
-        `${BASE_URL}/api/members/find-email?name=${name}&phone=${phone}`
-      );
-      console.log(response.data);
-      const code = response.data.code;
-      if (code === "이메일 찾기 성공 코드") {
-        // 이메일이 존재하고, 찾았을 때
-        alert(`Email: ${response.data.body}`);
-      } else if (code === "이름 형식 불일치") {
-        // alert("이름을 형식에 맞게 입력해주세요. 한/영 2자이상 16자 이하"); //debug용
-        setFindEmailResult(
-          "이름을 형식에 맞게 입력해주세요. 한/영 2자이상 16자 이하"
-        );
-      } else if (code === "번호 미인증") {
-        //번호 없는 사람 -> 회원가입 안한 사람?
-        // alert("이메일이 존재하지 않습니다."); //debug용
-        setFindEmailResult("이메일이 존재하지 않습니다. 번호미인증");
-      } else {
-        alert("이메일이 존재하지 않습니다." + response.data.code);
-      }
-    } catch (error) {
-      console.error("Error while finding email:", error);
-      alert("이메일을 찾는 중 오류가 발생했습니다.");
     }
   };
 
@@ -281,7 +222,7 @@ const FindEmail = () => {
         {!isVerificationFieldVisible && (
           <ButtonLarge
             button={{
-              disabled: name.length < 1 || phone.length < 12,
+              disabled: name.length < 1 || phone.length < 11,
               onClick: showVerificationField,
               title: "인증번호 요청",
             }}
@@ -333,33 +274,20 @@ const FindEmail = () => {
               <ButtonLarge
                 button={{
                   disabled: phoneVeri.length < 6,
-                  // onClick: isTimeout ? showVerificationField : verifyPhoneCode,
-                  onClick: () => {
-                    if (isTimeout) {
-                      setPhoneVeri(""); // 인증번호 입력 필드를 초기화
-                      setIsTimeout(false); // isTimeout 상태를 false로 변경
-                      showVerificationField(); // 인증번호 요청 함수 호출
-                    } else {
-                      verifyPhoneCode(); // 인증번호 확인 함수 호출
-                    }
-                  },
+                  onClick: isTimeout ? showVerificationField : verifyPhoneCode,
                   title: isTimeout ? "인증번호 요청" : "인증하기",
                 }}
               ></ButtonLarge>
-              {/* ㄴ 인증하기 버튼 -> 시간초과되면 인증번호 요청 버튼으로 바뀜*/}
             </>
           )}
         </Div>
       </div>
 
-      {/* <SubmitButtonContainer> */}
-      <Button type="submit" onClick={handleSubmit}>
-        이메일 찾기
-      </Button>
-      {/* </SubmitButtonContainer> */}
-      <h3>{findEmailResult}</h3>
+      <SubmitButtonContainer>
+        <Button type="submit" onClick={handleSubmit}>
+          이메일 찾기
+        </Button>
+      </SubmitButtonContainer>
     </Container>
   );
 };
-
-export default FindEmail;
