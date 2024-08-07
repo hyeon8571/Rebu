22930912860e8b31c2644e69559ccbf044e8c213
hyeon8571 <97227876+hyeon8571@users.noreply.github.com>
2024-08-07 16:07:14 +0900

    Update SecurityConfig.java

--- back/src/main/java/com/rebu/security/config/SecurityConfig.java
+++ back/src/main/java/com/rebu/security/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 
                                 CorsConfiguration configuration = new CorsConfiguration();
 
-                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedOrigins(Collections.singletonList("https://www.rebu.kro.kr"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
                                 configuration.setAllowedHeaders(Collections.singletonList("*"));
