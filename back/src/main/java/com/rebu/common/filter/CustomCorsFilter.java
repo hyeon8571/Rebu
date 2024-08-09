4f5bc11d024e6fcd2e541d9c7d0d4b82ea05c827
Lee Yuseung <105223190+yuseung0429@users.noreply.github.com>
2024-08-09 09:37:35 +0900

    Update CustomCorsFilter.java

--- back/src/main/java/com/rebu/common/filter/CustomCorsFilter.java
+++ back/src/main/java/com/rebu/common/filter/CustomCorsFilter.java
@@ -21,7 +21,6 @@ public class CustomCorsFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
