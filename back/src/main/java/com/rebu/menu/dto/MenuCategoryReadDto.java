a6a2fba8b5279eab724fc6eff44479be7e8c2294
Kimjongdeok11 <156400494+Kimjongdeok11@users.noreply.github.com>
2024-08-10 11:17:08 +0900

    Fix #93 menu 카테고리 조회 (#96)

---
+++ back/src/main/java/com/rebu/menu/dto/MenuCategoryReadDto.java
@@ -0,0 +1,21 @@
+package com.rebu.menu.dto;
+
+import com.rebu.menu.entity.Menu;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+public class MenuCategoryReadDto {
+    private List<String> categoryList;
+
+    public static MenuCategoryReadDto from(List<String> categoryList) {
+        return MenuCategoryReadDto.builder()
+                .categoryList(categoryList)
+                .build();
+    }
+}
--- back/src/main/java/com/rebu/menu/repositoy/MenuRepository.java
+++ back/src/main/java/com/rebu/menu/repositoy/MenuRepository.java
@@ -1,10 +1,15 @@
 package com.rebu.menu.repositoy;
 
 import com.rebu.menu.entity.Menu;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findByEmployeeId(Long id);
+
+    @Query("SELECT m.category FROM Menu m WHERE m.employee.nickname = :nickname")
+    List<String> findMenuCategoriesByEmployeeNickname(@Param("nickname") String nickname);
 }
