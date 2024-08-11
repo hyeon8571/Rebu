06f49d779f11323d91f245ec1ab70027e4ae6dbe
Lee Yuseung <105223190+yuseung0429@users.noreply.github.com>
2024-08-12 04:07:12 +0900

    Feature #117 프로필 검색, 해시태그 검색 정렬 (#118)

--- back/src/main/java/com/rebu/feed/repository/HashtagRepository.java
+++ back/src/main/java/com/rebu/feed/repository/HashtagRepository.java
@@ -10,11 +10,18 @@ import java.util.List;
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
     void deleteByFeedId(Long feedId);
 
-    @Query(value = """
-                   SELECT new com.rebu.feed.dto.HashtagCountDto(h.tag, COUNT(*))
-                   FROM Hashtag h
-                   WHERE h.tag LIKE :keyword%
-                   GROUP BY h.tag
-                   """)
+    @Query("""
+           SELECT new com.rebu.feed.dto.HashtagCountDto(h.tag, COUNT(*))
+           FROM Hashtag h
+           WHERE h.tag LIKE %:keyword%
+           GROUP BY h.tag
+           ORDER BY CASE
+           WHEN h.tag = :keyword THEN 0
+           WHEN h.tag LIKE :keyword% THEN 1
+           WHEN h.tag LIKE %:keyword THEN 2
+           WHEN h.tag LIKE %:keyword% THEN 3
+           ELSE 4
+           END, COUNT(*) DESC
+           """)
     List<HashtagCountDto> countHashtagsByPrefix(String keyword);
 }
