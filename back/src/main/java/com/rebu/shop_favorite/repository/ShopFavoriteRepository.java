29cd0d1b286db02b0bbc1522993c4de9e7d9974a
hyeon8571 <w01085914442@gmail.com>
2024-08-16 12:08:33 +0900

    chore: rating avg 수정

--- back/src/main/java/com/rebu/shop_favorite/repository/ShopFavoriteRepository.java
+++ back/src/main/java/com/rebu/shop_favorite/repository/ShopFavoriteRepository.java
@@ -16,7 +16,7 @@ public interface ShopFavoriteRepository extends JpaRepository<ShopFavorite, Shop
            s.imageSrc,
            s.nickname,
            s.name,
-           AVG(rv.rating / 2),
+           AVG(rv.rating),
            s.isPrivate,
            s.introduction,
            COUNT(DISTINCT f.id),
